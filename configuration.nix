03231db60cfa7d87223953220078b4011a8d5a9e
ipillyx <shaunpilgrimx@gmail.com>
2025-08-05 15:44:26 +0100

    Initial NixOS config backup

---
+++ configuration.nix
@@ -0,0 +1,138 @@
+{ config, lib, pkgs, ... }:
+
+{
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+
+    ];
+
+  # Forcefully disable GRUB in case any module tries to enable it
+  boot.loader.grub.enable = lib.mkForce false;
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+
+  # Bootloader.  boot.loader.systemd-boot.enable = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  networking.nameservers = [ "1.1.1.1" "8.8.8.8" ];
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/London";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_GB.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_GB.UTF-8";
+    LC_IDENTIFICATION = "en_GB.UTF-8";
+    LC_MEASUREMENT = "en_GB.UTF-8";
+    LC_MONETARY = "en_GB.UTF-8";
+    LC_NAME = "en_GB.UTF-8";
+    LC_NUMERIC = "en_GB.UTF-8";
+    LC_PAPER = "en_GB.UTF-8";
+    LC_TELEPHONE = "en_GB.UTF-8";
+    LC_TIME = "en_GB.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the Budgie Desktop environment.
+  services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.desktopManager.budgie.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "gb";
+    variant = "";
+  };
+
+  # Configure console keymap
+  console.keyMap = "uk";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable PipeWire for sound
+  services.pipewire = {
+    enable = true;
+    audio.enable = true;
+    pulse.enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+  };
+  security.rtkit.enable = true;
+
+
+  # Enable sound with pipewire.  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.shaun = {
+    isNormalUser = true;
+    description = "shaun";
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    packages = with pkgs; [
+      thunderbird
+      openconnect
+      networkmanager
+      networkmanager-openconnect
+      networkmanagerapplet
+      cifs-utils
+      remmina
+      discord
+      git
+    ];
+  };
+
+  # Enable automatic login for the user.
+  services.displayManager.autoLogin.enable = true;
+  services.displayManager.autoLogin.user = "shaun";
+
+  # Install Apps
+  programs.firefox.enable = true;
+  programs.nix-ld.enable = true;
+
+  # Allow unfree packages (required for Steam)
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile
+  environment.systemPackages = with pkgs; [
+    wget
+    vscode
+    steam
+    docker-compose
+    docker
+   appimage-run];
+
+  # Docker service enabled
+  virtualisation.docker.enable = true; 
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+  services.openssh.settings.PermitRootLogin = "no";
+
+  # AMD GPU driver
+  services.xserver.videoDrivers = [ "amdgpu" ];
+
+  # OpenGL and Vulkan support
+  hardware.graphics.enable = true;
+  hardware.graphics.enable32Bit = true;
+  hardware.graphics.extraPackages = with pkgs; [
+    vulkan-loader
+    vulkan-tools
+  ];
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken.
+  system.stateVersion = "25.05"; # Did you read the comment?
+}
